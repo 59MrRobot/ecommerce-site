82798c5a4eb3370e0d74aad6bfd6f1aebbbf71c2
Swazi Kunene <kuneneswazi18@gmail.com>
2023-01-25 13:27:30 +0200

    implemented products page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import { Home } from './pages/Home';
+import { ProductList } from './pages/ProductList';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <ProductList />
     </div>
   );
 }
